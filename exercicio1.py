8ccc9cc5b7a672a0a7b8f7ca875fbcbf6a5906d8
Thiago Mendes <thiagomendessan293@gmail.com>
2019-08-13 17:47:40 -0300

    ADicionar Python

---
+++ exercicio1.py
@@ -0,0 +1,38 @@
+# nome = input('Digite seu nome: ')
+# hora = int(input('Digite a hora trabalhada: '))
+# porHora = float(input('Digite o valor ganho por hora: '))
+
+# sal = hora * porHora
+
+# print('Salario de ' + nome + ': R$' + str(sal))
+
+dia = int(input('Digite número de 1 a 7 e descubra o dia da semana: '))
+
+if dia == 1 :
+    print('É Domingo')
+
+elif dia == 2 :
+    print('É Segunda')
+
+elif dia == 3 :
+    print('É Terça')
+
+elif dia == 4 :
+    print('É Quarta')
+
+elif dia == 5 :
+    print('É Quinta')
+
+elif dia == 6 :
+    print('É Sexta')
+
+elif dia == 7 :
+    print('É Sábado')
+
+else:
+    
+
+
+
+
+
